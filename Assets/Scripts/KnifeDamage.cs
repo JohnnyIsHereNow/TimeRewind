ee8f577dda059a4aa42e98048bc28d9387740ffd
Iubisoft <iubisoft.com@gmail.com>
2014-11-02 17:54:16 +0100

    Platform and emeny nr 3
    I created the assets fot platforms and enemy #3, also i created a
    switch. Now i need to make the scripts for them and to create the joints
    for 'moving platforms'

--- Assets/Scripts/KnifeDamage.cs
+++ Assets/Scripts/KnifeDamage.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class KnifeDamage : MonoBehaviour {
 	private int damage=10;
 	private GameObject enemy;
+	private Enemy2Script e2s;
 	private EnemyOne eo;
 	// Use this for initialization
 	void Start () {
@@ -13,10 +14,15 @@ public class KnifeDamage : MonoBehaviour {
 	void Update () {
 	}
 	void OnTriggerEnter2D(Collider2D col){
-		if (col.gameObject.tag == "Enemy" && PlayerController.attackWithKnife) {
+		if (col.gameObject.tag == "Enemy" && col.gameObject.name=="Enemy1" && PlayerController.attackWithKnife) {
 			enemy=col.gameObject;
 			eo=enemy.gameObject.GetComponent<EnemyOne>();
 			eo.setLife(damage);
+		}
+		if (col.gameObject.tag == "Enemy" && col.gameObject.name=="Enemy2" && PlayerController.attackWithKnife) {
+			enemy=col.gameObject;
+			e2s=enemy.gameObject.GetComponent<Enemy2Script>();
+			e2s.setLife(damage);
 		}
 	}
 }
---
+++ Assets/Scripts/MovePlatformUpDown.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlatformUpDown: MonoBehaviour {
+	private float movingSpeed;
+	private float maxGone;
+	private float gone;
+	private GameObject player;
+	// Use this for initialization
+	void Start () {
+		movingSpeed = -0.1f;
+		maxGone = 20;
+		gone = 0;
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void OnCollisionStay2D(Collision2D col){
+				if (col.gameObject.tag == "Player" || col.gameObject.tag == "Enemy") {
+						col.transform.Translate (new Vector2 (0,movingSpeed));
+
+				}
+		if(col.gameObject.tag=="Player" || col.gameObject.transform.position.y<transform.position.y){
+			movingSpeed=0.1f;
+		}
+		}
+	void Update () {
+				transform.Translate (new Vector2 (0,movingSpeed));
+				gone += 0.1f;
+
+				if (gone >= maxGone)
+		{
+						gone = 0;
+			if(movingSpeed==-0.1f)
+						movingSpeed = 0.1f;
+			else movingSpeed=-0.1f;
+		}
+	}
+}
